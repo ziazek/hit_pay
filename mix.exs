792f17bc98d0e1d420f5a408c2b33fd3bcb3dfc3
Zek Chak <ziazek@gmail.com>
2023-10-07 15:08:53 +0800

    downgrade plug_crypto

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule HitPay.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:jason, "~> 1.4"},
       {:httpoison, "~> 2.1.0"},
-      {:plug_crypto, "~> 2.0"},
+      {:plug_crypto, "~> 1.2"},
       {:ex_doc, "~> 0.30.6", only: :dev}
     ]
   end
